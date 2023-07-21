b47567c72e71775f35e6c9e140de886e100199c8
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-20 23:12:00 -0300

    empezando proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Titulo from "./components/Titulo";
 
 function App() {
-  
-
   return (
     <>
-      
+      <Titulo></Titulo>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+const Titulo = () => {
+  return (
+    <article>
+      <h1>hello world</h1>
+    </article>
+  );
+};
+
+export default Titulo;
